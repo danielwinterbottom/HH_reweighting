52f30646c7682c20575376a93fa89d2ec083c279
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2023-11-10 09:33:51 +0000

    first version of batch submission script, not working at the moment

---
+++ scripts/submit_ic_batch_jobs.py
@@ -0,0 +1,57 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--events_per_job', help= 'Number of events per job', default=10000, type=int)
+parser.add_argument('--total_events', help= 'Total number of events to produce', default=1000000, type=int)
+parser.add_argument('--input', '-i', help= 'Name of input madgraph directory')
+args = parser.parse_args()
+
+current_dir=os.getcwd()
+input = args.input
+nevents = args.events_per_job
+name = args.input.split('/')[-1]
+
+# first we need to make a gridpack
+#os.system('cd %(current_dir)s/%(input)s; { echo 0; echo set nevents 100; echo set gridpack True; } | python2 bin/generate_events %(name)s; cd %(current_dir)s' % vars())
+
+out_string = '#!/bin/sh\n\
+cd %(current_dir)s/batch_job_outputs/%(name)s/job_output_${SGE_TASK_ID}\n\
+rm -rf %(current_dir)s/batch_job_outputs/%(name)s/job_output_${SGE_TASK_ID}/*\n\
+tar -xvf %(current_dir)s/%(input)s/%(name)s_gridpack.tar.gz\n\
+./run.sh %(nevents)i ${SGE_TASK_ID}\n\
+mv events.lhe.gz events_${SGE_TASK_ID}.lhe.gz\n\
+echo 0 | madevent/bin/madevent reweight events_${SGE_TASK_ID}.lhe.gz\n\
+rm -rf madevent run.sh py.py' % vars()
+
+# make an output directory
+os.system('mkdir -p batch_job_outputs/%(name)s/logs' % vars())
+
+for i in range(1,args.total_events/args.events_per_job+1):
+    # make an directory for each job where the gridpack will be untarred and the output will be stored
+    os.system('mkdir -p %(current_dir)s/batch_job_outputs/%(name)s/job_output_%(i)i' % vars())
+
+    #out_string += 'if [ $SGE_TASK_ID == %(i)i ]; then { echo 0; echo set run_card iseed %(i)i; echo set nevents %(nevents)i; } | python2 bin/generate_events parajob_output_%(i)i &> %(current_dir)s/jobs/parajob_%(name)s_%(i)i.log; fi\n' % vars()
+#    out_string += 'if [ $SGE_TASK_ID == %(i)i ]; then\n\
+#\tcd %(current_dir)s/batch_job_outputs/%(name)s/job_output_%(i)i\n\
+#\trm -rf %(current_dir)s/batch_job_outputs/%(name)s/job_output_%(i)i/*\n\
+#\tcp %(current_dir)s/%(input)s/%(name)s_gridpack.tar.gz .\n\
+#\ttar -xvf %(name)s_gridpack.tar.gz\n\
+#\t./run.sh %(nevents)i %(i)i\n\
+#\tmv events.lhe.gz events_%(i)i.lhe.gz\n\
+#\techo 0 | madevent/bin/madevent reweight events_%(i)i.lhe.gz\n\
+#\trm -rf madevent run.sh %(name)s_gridpack.tar.gz py.py\n\
+#fi\n' % vars()
+#\t./run.sh %(nevents)i %(i)i &> run.log\n\
+#\techo 0 | madevent/bin/madevent reweight events_%(i)i.lhe.gz &> reweight.log\n\
+
+
+print 'Writing job file: jobs/parajob_%(name)s.sh' % vars()
+with open("jobs/parajob_%(name)s.sh" % vars(), "w") as output_file:
+    output_file.write(out_string)
+os.system('chmod +x jobs/parajob_%(name)s.sh' % vars())
+
+total = int(args.total_events/args.events_per_job)
+print 'Submitting jobs'
+os.system('qsub -q hep.q -l h_rt=300 -t 1-%(total)i:1 jobs/parajob_%(name)s.sh' % vars())
+#os.system('qsub -e /dev/null -o /dev/null -q hep.q -l h_rt=300 -t 1-%(total)i:1 jobs/parajob_%(name)s.sh' % vars())
