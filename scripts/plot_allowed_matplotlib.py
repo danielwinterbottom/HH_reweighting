803f61f8241a1418859f4e7e81eac607abc5d0b3
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2024-03-28 16:11:29 +0000

    adding script to make plots

--- scripts/plot_allowed_matplotlib.py
+++ scripts/plot_allowed_matplotlib.py
@@ -14,6 +14,8 @@ points_files = ['oks_fullscan_ht_new.dat', 'oks_minus_ht_new.dat', 'oks_smallsa_
 data_1 = {}
 data_2 = {}
 
+data_failed_1 = {}
+
 wmass_x = []
 wmass_y = []
 wmass_y_lower = []
@@ -36,13 +38,32 @@ for points_file in points_files:
             sina = float(split_line[2])
             tanb = float(split_line[3])
     
+
+            if not split_line[-2]=='1':
+
+                if sina>0 :
+                    if mH in data_failed_1:
+                        data_failed_1[mH] = min(data_failed_1[mH], sina)
+                    else:
+                        data_failed_1[mH] = sina
+            
             passed = split_line[-1]=='1' and split_line[-2]=='1'
-            if not passed: continue
+
+            if not passed:
+                continue
 
             if (mH,sina) not in data_1 or tanb > data_1[(mH,sina)]:
                 data_1[(mH,sina)] = tanb
             if (mH,tanb) not in data_2 or abs(sina) > abs(data_2[(mH,tanb)]):
                 data_2[(mH,tanb)] = abs(sina)
+
+#data_failed_1_x = []
+#data_failed_1_y = []
+#for key in data_failed_1:
+#  mH = key
+#  sina = data_failed_1[key]
+#  data_failed_1_x.append(mH)
+#  data_failed_1_y.append(sina)
 
 
 x1 = []
@@ -68,13 +89,13 @@ for key in data_2:
   y2.append(tanb)
   z2.append(max_sina)
 
-cmap = 'Blues'
-#cmap = 'viridis'
-#cmap = 'cividis'
+#cmap = 'Blues'
+cmap = 'viridis_r'
+#cmap = 'cividis_r'
 #cmap = 'Purples'
 
 # plot sina vs mH
-plt.scatter(x1, y1, c=z1, cmap=cmap, marker='o',s=20)
+plt.scatter(x1, y1, c=z1, cmap=cmap, marker='o',s=10)
 ax = plt.gca()
 ax.tick_params(axis='both', which='both', direction='in',labelsize=12,length=8)
 ax.set_xlim([min(x1), max(x1)])
@@ -85,6 +106,7 @@ ax.yaxis.set_minor_locator(AutoMinorLocator())
 ax.xaxis.set_minor_locator(AutoMinorLocator())
 ax.tick_params(axis='both', which='minor', length=4, width=1)
 ax.fill_between(wmass_x, wmass_y, 5, color='red', alpha=0.3,label=r'$m_{\mathrm{W}}$ constraint')
+#ax.fill_between(data_failed_1_x, data_failed_1_y, 5, color='blue', alpha=0.3,label=r'test')
 ax.fill_between(wmass_x, wmass_y_lower, -5, color='red', alpha=0.3)
 ax.legend(loc='upper left', fontsize=14, bbox_to_anchor=(0., 1.15), ncol=2, frameon=False)
 cb = plt.colorbar()
@@ -99,7 +121,7 @@ plt.savefig('allowed_points_sina_vs_mH_maxtanb.pdf')
 plt.close()
 
 # plot tanb vs mH
-plt.scatter(x2, y2, c=z2, cmap=cmap, marker='o',s=20)
+plt.scatter(x2, y2, c=z2, cmap=cmap, marker='o',s=10)
 ax = plt.gca()
 ax.tick_params(axis='both', which='both', direction='in',labelsize=12, length=8)
 ax.set_xlim([min(x1), max(x1)])
