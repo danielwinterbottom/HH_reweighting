805f800a85a3be3c167aa207741824673c4c7e87
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2025-01-20 15:57:47 +0000

    d_min computations working but need to figure out how to implement this for numerical results

--- scripts/ReconstructTaus.py
+++ scripts/ReconstructTaus.py
@@ -3,7 +3,54 @@ import numpy as np
 from scipy.optimize import minimize
 import math
 
-def ChangeFrame(taun, taunvis, vec):
+def ChangeFrame(taun, taunvis, vec, reverse=False):
+    '''
+    Rotate the coordinate axis as follows:
+    z-direction should be direction of the tau-
+    xz-axis should adjust the direction so that the h- lies on the x-z plane, in the +ve x-direction.
+
+    Args:
+        taun (TVector3): The tau- vector.
+        taunvis (TVector3): The visible tau- vector (h-).
+        vec (TVector3): The vector to rotate.
+        reverse (bool): If True, applies the inverse transformation to return to the original frame.
+
+    Returns:
+        TVector3: The rotated vector.
+    '''
+    vec_new = vec.Clone()
+    taunvis_copy = taunvis.Clone()
+    
+    # Define the rotation angles to allign with tau- direction
+    theta = taun.Theta()
+    phi = taun.Phi()
+
+    # Rotate taunvis to get second phi angle for rotation
+    taunvis_copy.RotateZ(-phi)
+    taunvis_copy.RotateY(-theta)
+    phi2 = taunvis_copy.Phi()  # This is the phi angle of the rotated taunvis vector 
+
+    if reverse:
+        # Reverse transformation: Undo the rotations in reverse order
+
+        vec_new.RotateZ(phi2)
+        vec_new.RotateY(theta)
+        vec_new.RotateZ(phi)
+    else:
+        # Forward transformation: Apply the rotations
+
+        vec_new.RotateZ(-phi)
+        vec_new.RotateY(-theta)
+        vec_new.RotateZ(-phi2)
+
+        # Check that h- is in the +ve x-direction
+        if taunvis == vec and vec_new.X() < 0:
+            raise ValueError("h- not pointing in the +ve x direction")
+
+    return vec_new
+
+
+def ChangeFrameOld(taun, taunvis, vec, reverse=False):
     '''
     Rotate the coordinate axis as follows:
     z-direction should be direction of the tau-
@@ -32,7 +79,7 @@ def ChangeFrame(taun, taunvis, vec):
     return vec_new
 
 
-def objective(vars, P_Z, P_taupvis, P_taunvis):
+def objective(vars, P_Z, P_taupvis, P_taunvis, d_min_reco=None):
     mtau = 1.775538 
     nubar_x, nubar_y, nubar_z, nu_x, nu_y, nu_z = vars
     P_taupnu = ROOT.TLorentzVector(nubar_x, nubar_y, nubar_z, (nubar_x**2+nubar_y**2+nubar_z**2)**.5) 
@@ -48,13 +95,28 @@ def objective(vars, P_Z, P_taupvis, P_taunvis):
     eq5 = mom_sum.Y() - P_Z.Y()
     eq6 = mom_sum.Z() - P_Z.Z()
 
+    if d_min_reco: # if the reco d_min is supplied then also implement IP constraint
+        P_taunvis_new = ChangeFrame(P_taunnu, P_taunvis, P_taunvis)
+        P_taupvis_new = ChangeFrame(P_taunnu, P_taunvis, P_taupvis)
+
+        d_min = PredictDmin(P_taunvis_new, P_taupvis_new, ROOT.TVector3(0.,0.,-1.)).Unit()
+        d_min = ChangeFrame(P_taunnu, P_taunvis, d_min, reverse=True)
+
+        #eq7 = math.acos(d_min.Unit().Dot(d_min_reco.Unit()))
+        dot_product = d_min.Unit().Dot(d_min_reco.Unit())
+        #clamped_dot = max(-1.0, min(1.0, dot_product))
+        #eq7 = math.acos(clamped_dot)
+        eq7 = -dot_product
+
+        return (eq1**2 + eq2**2 + eq3**2 + eq4**2 + eq5**2 + eq6**2) #* eq7 # this constant should be of order O(100) scale to match other constraints but this can be tuned
+
     return eq1**2 + eq2**2 + eq3**2 + eq4**2 + eq5**2 + eq6**2
 
-def ReconstructTau(P_Z, P_taupvis, P_taunvis, verbose=False):
+def ReconstructTau(P_Z, P_taupvis, P_taunvis, d_min_reco=None, verbose=False):
     initial_guess = [P_taupvis.X(), P_taupvis.Y(), P_taupvis.Z(), P_taunvis.X(), P_taunvis.Y(), P_taunvis.Z()]
 
     # Call the optimizer
-    result = minimize(objective, initial_guess, args=(P_Z, P_taupvis, P_taunvis))
+    result = minimize(objective, initial_guess, args=(P_Z, P_taupvis, P_taunvis, d_min_reco))
 
     if verbose:
         if result.success:
@@ -159,11 +221,6 @@ def ReconstructTauAnalytically(P_Z, P_taupvis, P_taunvis):
 
     q = compute_q(P_Z*P_Z,P_Z, P_taupvis, P_taunvis)
 
-    #print('check q:') # these should all equal 0:
-    #print(q.Dot(P_Z))
-    #print(q.Dot(P_taupvis))
-    #print(q.Dot(P_taunvis))
-
     x = P_Z * P_taupvis
     y = P_Z * P_taunvis
     z = P_taupvis * P_taunvis
@@ -208,7 +265,125 @@ def GetGenImpactParam(primary_vtx, secondary_vtx, part_vec):
     sv = secondary_vtx - primary_vtx # secondary vertex wrt primary vertex    
     unit_vec = part_vec.Unit() # direction of particle / track
     ip = (sv - sv.Dot(unit_vec)*unit_vec)
-    return ip    
+    return ip  
+
+def FindDMin(p1, d1, p2, d2):
+    """
+    Find the vector pointing from the closest point on Line 1 to the closest point on Line 2 using ROOT classes.
+
+    Args:
+        p1 (TVector3): A point on Line 1.
+        d1 (TVector3): Direction vector of Line 1.
+        p2 (TVector3): A point on Line 2.
+        d2 (TVector3): Direction vector of Line 2.
+
+    Returns:
+        TVector3: Vector pointing from the closest point on Line 1 to the closest point on Line 2.
+    """
+    # Normalize direction vectors
+    d1 = d1.Unit()
+    d2 = d2.Unit()
+
+    # Compute cross product and its magnitude squared
+    cross_d1_d2 = d1.Cross(d2)
+    denom = cross_d1_d2.Mag2()
+
+    # Handle parallel lines (cross product is zero)
+    if denom == 0:
+        raise ValueError("The lines are parallel or nearly parallel.")
+
+    # Compute t1 and t2 using determinant approach
+    dp = p2 - p1
+    t1 = (dp.Dot(d2.Cross(cross_d1_d2))) / denom
+    t2 = (dp.Dot(d1.Cross(cross_d1_d2))) / denom
+
+    # Closest points on each line
+    pca1 = p1 + d1 * t1
+    pca2 = p2 + d2 * t2
+
+    # Vector pointing from closest point on Line 1 to Line 2
+    return pca2 - pca1
+
+class FindDMinNumerical:
+    """
+    Find the vector pointing from the closest point on Line 1 to the closest point on Line 2 using ROOT classes.
+
+    Args:
+        p1 (TVector3): A point on Line 1.
+        d1 (TVector3): Direction vector of Line 1.
+        p2 (TVector3): A point on Line 2.
+        d2 (TVector3): Direction vector of Line 2.
+
+    Returns:
+        TVector3: Vector pointing from the closest point on Line 1 to the closest point on Line 2.
+    """
+    def __init__(self, p1, d1, p2, d2):
+        self.p1 = p1
+        self.d1 = d1
+        self.p2 = p2
+        self.d2 = d2
+
+    def objective(self, t):
+        """
+        Objective function to minimize the distance between the two lines.
+
+        Args:
+            t (np.array): Array of parameters to minimize.
+
+        Returns:
+            float: Distance between the two lines.
+        """
+        # Compute the points on each line
+        term1 = self.d1.Clone()
+        term2 = self.d2.Clone()
+        term1 *= t[0]
+        term2 *= t[1]
+        l1 = self.p1 + term1  # Point on Line 1
+        l2 = self.p2 + term2  # Point on Line 2
+
+        # Compute the distance between the two points
+        return (l1 - l2).Mag2()
+
+    def minimize(self):
+        """
+        Minimize the distance between the two lines.
+
+        Returns:
+            TVector3: Vector pointing from the closest point on Line 1 to the closest point on Line 2.
+        """
+        # Initial guess for the parameters
+        t0 = np.array([0., 0.])
+
+        # Call the optimizer
+        result = minimize(self.objective, t0)
+
+        # Extract the optimized parameters
+        t_opt = result.x
+
+        # Compute the closest points
+        term1 = self.d1.Clone()
+        term2 = self.d2.Clone()
+        term1 *= t_opt[0]
+        term2 *= t_opt[1]
+
+        l1_closest = self.p1 + term1
+        l2_closest = self.p2 + term2
+
+        # Return the vector pointing from the closest point on Line 1 to Line 2
+        return l2_closest - l1_closest
+
+def PredictDmin(P_taunvis, P_taupvis, d=None):
+    if d is None: d_over_l = ROOT.TVector3(0.,0.,-1.) # by definition in the rotated coordinate frame
+    else: d_over_l = d
+    n_n = P_taunvis.Vect().Unit()
+    n_p = P_taupvis.Vect().Unit()
+    fact1 = (d_over_l.Dot(n_p)*n_n.Dot(n_p) - d_over_l.Dot(n_n))/(1.-(n_n.Dot(n_p))**2)
+    fact2 = (d_over_l.Dot(n_n)*n_n.Dot(n_p) - d_over_l.Dot(n_p))/(1.-(n_n.Dot(n_p))**2)
+    term1 = n_n*fact1
+    term2 = n_p*fact2
+
+    d_min = d_over_l + (term1 + term2)
+    return d_min
 
 if __name__ == '__main__':
 
@@ -216,7 +391,7 @@ if __name__ == '__main__':
     tree = f.Get('tree')
     count_total = 0
     count_correct = 0
-    for i in range(1,10):
+    for i in range(1,1000):
         count_total+=1
         tree.GetEntry(i)
         P_taup_true = ROOT.TLorentzVector(tree.taup_px, tree.taup_py, tree.taup_pz, tree.taup_e) 
@@ -234,14 +409,16 @@ if __name__ == '__main__':
         VERTEX_taun = ROOT.TVector3(tree.taun_pi1_vx, tree.taun_pi1_vy, tree.taun_pi1_vz) # in mm
 
         l_true = abs((VERTEX_taup-VERTEX_taun).Mag())
+        d_true = VERTEX_taup-VERTEX_taun
 
         IP_taup = GetGenImpactParam(ROOT.TVector3(0.,0.,0.),VERTEX_taup, P_taupvis.Vect())
         IP_taun = GetGenImpactParam(ROOT.TVector3(0.,0.,0.),VERTEX_taun, P_taunvis.Vect())
+    
+        finder = FindDMinNumerical(VERTEX_taun, P_taunvis.Vect().Unit(), VERTEX_taup, P_taupvis.Vect().Unit())
+        d_min_numeric = finder.minimize()
 
-        #TODO: this is wrong as you need to get minimum distance between 2 tracks not difference between bin distance between tracks and PV!!!
-        d_min_reco = IP_taup - IP_taun
-
-        P_taup_reco, P_taun_reco = ReconstructTau(P_Z, P_taupvis, P_taunvis, verbose=False)
+        d_min_reco = FindDMin(VERTEX_taun, P_taunvis.Vect().Unit(), VERTEX_taup, P_taupvis.Vect().Unit())
+        P_taup_reco, P_taun_reco = ReconstructTau(P_Z, P_taupvis, P_taunvis, d_min_reco, verbose=False)
    
         solutions = ReconstructTauAnalytically(P_Z, P_taupvis, P_taunvis)
 
@@ -272,7 +449,7 @@ if __name__ == '__main__':
         P_taunvis_2.Boost(-solutions[0][1].BoostVector())
         P_taunvis_3.Boost(-solutions[1][1].BoostVector())       
 
-        if i < 10 and False: # only print a few events
+        if i < 10 and True: # only print a few events
 
             print('\n---------------------------------------')
             print('Event %i' %i)
@@ -292,64 +469,39 @@ if __name__ == '__main__':
             print('tau+:', solutions[1][0].X(), solutions[1][0].Y(), solutions[1][0].Z(), solutions[1][0].T(), solutions[1][0].M())
             print('tau-:', solutions[1][1].X(), solutions[1][1].Y(), solutions[1][1].Z(), solutions[1][1].T(), solutions[1][1].M())
     
-            print('\nboost to tau rest frames')
-            print('True:')
-            print('tau+:', P_taupvis_1.X(), P_taupvis_1.Y(), P_taupvis_1.Z(), P_taupvis_1.T(), P_taupvis_1.M())
-            print('tau-:', P_taunvis_1.X(), P_taunvis_1.Y(), P_taunvis_1.Z(), P_taunvis_1.T(), P_taunvis_1.M())
-            print('solution 1:')
-            print('tau+:', P_taupvis_2.X(), P_taupvis_2.Y(), P_taupvis_2.Z(), P_taupvis_2.T(), P_taupvis_2.M())
-            print('tau-:', P_taunvis_2.X(), P_taunvis_2.Y(), P_taunvis_2.Z(), P_taunvis_2.T(), P_taunvis_2.M())
-            print('solution 2:')
-            print('tau+:', P_taupvis_3.X(), P_taupvis_3.Y(), P_taupvis_3.Z(), P_taupvis_3.T(), P_taupvis_3.M())        
-            print('tau-:', P_taunvis_3.X(), P_taunvis_3.Y(), P_taunvis_3.Z(), P_taunvis_3.T(), P_taunvis_3.M())       
+#            print('\nboost to tau rest frames')
+#            print('True:')
+#            print('tau+:', P_taupvis_1.X(), P_taupvis_1.Y(), P_taupvis_1.Z(), P_taupvis_1.T(), P_taupvis_1.M())
+#            print('tau-:', P_taunvis_1.X(), P_taunvis_1.Y(), P_taunvis_1.Z(), P_taunvis_1.T(), P_taunvis_1.M())
+#            print('solution 1:')
+#            print('tau+:', P_taupvis_2.X(), P_taupvis_2.Y(), P_taupvis_2.Z(), P_taupvis_2.T(), P_taupvis_2.M())
+#            print('tau-:', P_taunvis_2.X(), P_taunvis_2.Y(), P_taunvis_2.Z(), P_taunvis_2.T(), P_taunvis_2.M())
+#            print('solution 2:')
+#            print('tau+:', P_taupvis_3.X(), P_taupvis_3.Y(), P_taupvis_3.Z(), P_taupvis_3.T(), P_taupvis_3.M())        
+#            print('tau-:', P_taunvis_3.X(), P_taunvis_3.Y(), P_taunvis_3.Z(), P_taunvis_3.T(), P_taunvis_3.M())       
             
+
+            print('d_min_reco = ', d_min_reco.Mag(), d_min_reco.Unit().X(), d_min_reco.Unit().Y(), d_min_reco.Unit().Z())
+
+            d_min = PredictDmin(P_taunvis, P_taupvis, d_true)
+            print('d_min_predict (true) = ', d_min.Mag(), d_min.Unit().X(), d_min.Unit().Y(), d_min.Unit().Z())
+
+
+            P_taunvis_new = ChangeFrame(P_taun_true, P_taunvis, P_taunvis)
+            P_taupvis_new = ChangeFrame(P_taun_true, P_taunvis, P_taupvis)
+
+            for i in range(0,2):
+                print ('solution %i' % (i+1))
+
+                P_taunvis_new = ChangeFrame(solutions[i][1], P_taunvis, P_taunvis)
+                P_taupvis_new = ChangeFrame(solutions[i][1], P_taunvis, P_taupvis)
+
+                d_min = PredictDmin(P_taunvis_new, P_taupvis_new, ROOT.TVector3(0.,0.,-1.))
+                print('d_min_predict_solution %i = ' % (i+1), d_min.Mag(), d_min.Unit().X(), d_min.Unit().Y(), d_min.Unit().Z())
+                d_min = P_taupvis_new = ChangeFrame(solutions[i][1], P_taunvis, d_min, reverse=True)
+                print('d_min_predict_solution %i (undo rotation) = ' % (i+1), d_min.Mag(), d_min.Unit().X(), d_min.Unit().Y(), d_min.Unit().Z())
+
+                print(d_min_reco.Unit().Dot(d_min.Unit()))
             print('correct solution found?', FoundCorrectSolution)
-
-        #GetOpeningAngle(P_taup_true, P_taupvis)
-        #P_taunvis_new =  ChangeFrame(P_taun_true, P_taunvis, P_taunvis)
-        #P_taupvis_new =  ChangeFrame(P_taup_true, P_taupvis, P_taupvis)
-        #P_taun_true_new =  ChangeFrame(P_taun_true, P_taunvis, P_taun_true)
-        #P_taup_true_new =  ChangeFrame(P_taup_true, P_taupvis, P_taup_true)
-
-        def PredictDmin(P_taunvis, P_taupvis):
-            d_over_l = ROOT.TVector3(0.,0.,-1.) # by definition in the rotated coordinate frame
-            n_n = P_taunvis.Vect().Unit()
-            n_p = P_taupvis.Vect().Unit()
-            fact1 = (d_over_l.Dot(n_p)*n_n.Dot(n_p) - d_over_l.Dot(n_n))/(1.-(n_n.Dot(n_p))**2)
-            fact2 = (d_over_l.Dot(n_n)*n_n.Dot(n_p) - d_over_l.Dot(n_p))/(1.-(n_n.Dot(n_p))**2)
-            term1 = n_n*fact1
-            term2 = n_p*fact2
-            d_min = d_over_l + (term1 - term2)
-            return d_min
-
-        for i in range(0,2):
-            print ('solution %i' % (i+1))
-
-#            #d_min = d_over_l + ( (d_over_l.Dot(n_p)*n_n.Dot(n_p) - d_over_l.Dot(n_n))*n_n - (d_over_l.Dot(n_n)*n_n.Dot(n_p) - d_over_l.Dot(n_p))*n_p )#*1./(1.-(n_n.Dot(n_p))**2)
-#            d_min_reco_new = ChangeFrame(solutions[i][1], P_taunvis, d_min_reco) # rotate reco d_min to same frame
-            P_taunvis_new = ChangeFrame(solutions[i][1], P_taunvis, P_taunvis)
-            P_taupvis_new = ChangeFrame(solutions[i][1], P_taunvis, P_taupvis)
-#            P_taun_new = ChangeFrame(solutions[i][1], P_taunvis, solutions[i][1])
-#            P_taup_new = ChangeFrame(solutions[i][1], P_taunvis, solutions[i][0])
-#            print("pi+:", P_taupvis_new.X(), P_taupvis_new.Y(), P_taupvis_new.Z())
-#            print("pi-:", P_taunvis_new.X(), P_taunvis_new.Y(), P_taunvis_new.Z())
-#            d_min = PredictDmin(P_taunvis_new, P_taupvis_new)
-
-#            theta_p = GetOpeningAngle(P_taup_new, P_taupvis_new)
-#            theta_n = GetOpeningAngle(P_taun_new, P_taunvis_new)
-            print('orig true tau-:', P_taun_true.X(), P_taun_true.Y(), P_taun_true.Z())
-            print('orig tau-:', solutions[i][1].X(), solutions[i][1].Y(), solutions[i][1].Z())
-            print('orig pi-:', P_taunvis.X(), P_taunvis.Y(), P_taunvis.Z())
-#            print('tau-:', P_taun_new.X(), P_taun_new.Y(), P_taun_new.Z())
-#            print('pi-:', P_taunvis_new.X(), P_taunvis_new.Y(), P_taunvis_new.Z())
-#            print('sintheta- = ', math.sin(theta_n))
-#            print('sintheta+ = ', math.sin(theta_p))
-#            print('pi-_x = ', P_taunvis_new.Vect().Unit().X())
-#            print('pi-_y = ', P_taunvis_new.Vect().Unit().Y())
-#            print('pi-_z = ', P_taunvis_new.Vect().Unit().Z())
-#            n_p = P_taupvis_new.Vect().Unit()
-#            print(d_min_reco.Dot(P_taupvis.Vect().Unit().Cross(P_taunvis.Vect().Unit())))
-#            #sinphi = n_p.Y()/math.sin(theta)
-#            #print ('sinphi =', sinphi)
 
     print('Found correct solution for %i / %i events = %.1f%%' % (count_correct, count_total, count_correct/count_total*100))
